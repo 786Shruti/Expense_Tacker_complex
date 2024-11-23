cfdddb5e2c0e4c44fd84502997f866a3f116ef3e
Shruti Satish Pol <143900541+786Shruti@users.noreply.github.com>
2024-11-23 09:57:16 +0530

    add_expense.php

--- add_expense.php
+++ add_expense.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once '../includes/functions.php'; // Corrected path
+include_once 'https://ec2-13-233-150-88.ap-south-1.compute.amazonaws.com/functions.php'; // Corrected path
 
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
